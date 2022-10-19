3368bdddd5a4d369e26f8399d3f8acaae8e2cbbb
Sinsl <sinsl@me.com>
2022-10-19 18:33:40 +0300

    ДЗ 4 модули fs, path

---
+++ homework-4-stream/analytics.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+
+let nameFile = '';
+process.argv.length > 2 ? nameFile = process.argv[2].replace(/[^a-zа-яё_-]/gi, '') + '.txt' : nameFile = 'log.txt';
+
+const filePath = path.join(__dirname, 'log', nameFile);
+//const filePath = require('./games').file;
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) throw Error(err);
+    const games = data.split('\n');
+    let rezult = [];
+    games.forEach(item => {
+        if (item.length > 0) rezult.push(item.split(''))
+    });
+    console.log(`Проведено серий игр: ${rezult.length}`);
+    let count = 0;
+    let win = 0;
+    rezult.forEach(item => {
+        item.forEach(elem => {
+            if (elem === '1') win++;
+            count++;
+        })
+    })
+    console.log(`Проведено партий: ${count}`);
+    console.log(`Из них выиграно: ${win}, проиграно ${count - win}`);
+    console.log(`Соотношение выиграный партий: ${Math.floor((win / count) * 100)}%`);
+})
---
+++ homework-4-stream/games.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const fs = require('fs');
+
+let nameFile = '';
+process.argv.length > 2 ? nameFile = process.argv[2].replace(/[^a-zа-яё_-]/gi, '') + '.txt' : nameFile = 'log.txt';
+
+const filePath = path.join(__dirname, 'log', nameFile);
+
+//module.exports.file = filePath;
+
+console.log('Игра запущена!');
+console.log(`Загадано 1 или 2. Для выхода введите 0.`);
+let content = '';
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.setPrompt("Введите ответ:\n");
+rl.prompt();
+rl.on( 'line' , (num) => {
+    let ran = Math.floor(1 + Math.random() * 2);
+    switch (num){
+        case '0':
+            console.log('Завершение игры.');
+            rl.close();
+            fs.appendFile(filePath, "\n", (err) => {
+                if (err) throw Error(err);
+            });
+        break;
+        case '1':
+        case '2':
+            if (Number(num) === ran) {
+                console.log(`Победа! Было загадано: ${ran}`);
+                console.log('Еще попытка. Введите ответ:');
+                content = '1';
+            } else {
+                console.log(`Поражение! Было загадано: ${ran}`);
+                console.log('Еще попытка. Введите ответ:');
+                content = '0';
+            }
+            fs.appendFile(filePath, content, (err) => {
+                if (err) throw Error(err);
+            });
+        break;
+        default:
+            console.log('Некорректный ввод');
+            console.log('Еще попытка. Введите ответ:');
+        break;
+    }
+});
